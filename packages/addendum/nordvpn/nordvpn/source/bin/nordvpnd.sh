79615210f9609a6222565f5b0d235935377bbb61
Kaspar Winckler <155115050+KasparWinckler@users.noreply.github.com>
2024-02-12 19:12:01 +0100

    nordvpn: version 3.17.1 and addon (2)

--- packages/addendum/nordvpn/nordvpn/source/bin/nordvpnd.sh
+++ packages/addendum/nordvpn/nordvpn/source/bin/nordvpnd.sh
@@ -3,6 +3,11 @@
 . /etc/profile
 oe_setup_addon service.nordvpn
 
+sysctl net.ipv4.ip_forward=1
+iptables -t nat -A POSTROUTING -j MASQUERADE -o eth0
+iptables -t nat -A POSTROUTING -j MASQUERADE -o wlan0
+iptables -t nat -A POSTROUTING -j MASQUERADE -o nordlynx
+
 SBIN="$(dirname ${0})/../sbin"
 chmod +x "${SBIN}"/*
 export PATH="${SBIN}:${PATH}"
