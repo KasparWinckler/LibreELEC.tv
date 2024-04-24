dcd73ff698cbbc5b09aa5bf0c4c4bcfaf2fc50a0
Kaspar Winckler <155115050+KasparWinckler@users.noreply.github.com>
2024-04-24 20:28:59 +0200

    zerotier-one: version 1.12.2 and addon (0)

--- packages/addendum/zerotier-one/package.mk
+++ packages/addendum/zerotier-one/package.mk
@@ -1,7 +1,7 @@
 PKG_NAME="zerotier-one"
 PKG_VERSION="1.12.2"
 PKG_SHA256="7c6512cfc208374ea9dc9931110e35f71800c34890e0f35991ea485aae66e31c"
-PKG_REV="1"
+PKG_REV="0"
 PKG_LICENSE="BSL1.1"
 PKG_SITE="https://www.zerotier.com/"
 PKG_URL="https://github.com/zerotier/ZeroTierOne/archive/${PKG_VERSION}.tar.gz"
