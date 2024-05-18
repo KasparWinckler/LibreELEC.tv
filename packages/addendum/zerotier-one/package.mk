df8c5833224f104b825e5a38c15aa95d72772936
Kaspar Winckler <155115050+KasparWinckler@users.noreply.github.com>
2024-05-18 09:10:13 +0200

    zerotier-one: version 1.14.0 and addon (1)

--- packages/addendum/zerotier-one/package.mk
+++ packages/addendum/zerotier-one/package.mk
@@ -1,7 +1,7 @@
 PKG_NAME="zerotier-one"
-PKG_VERSION="1.12.2"
-PKG_SHA256="7c6512cfc208374ea9dc9931110e35f71800c34890e0f35991ea485aae66e31c"
-PKG_REV="0"
+PKG_VERSION="1.14.0"
+PKG_SHA256="7191623a81b0d1b552b9431e8864dd3420783ee518394ac1376cee6aaf033291"
+PKG_REV="1"
 PKG_LICENSE="BSL1.1"
 PKG_SITE="https://www.zerotier.com/"
 PKG_URL="https://github.com/zerotier/ZeroTierOne/archive/${PKG_VERSION}.tar.gz"
@@ -16,11 +16,6 @@ PKG_SHORTDESC="${PKG_ADDON_NAME}: securely connect any device, anywhere"
 PKG_LONGDESC="${PKG_ADDON_NAME} (${PKG_VERSION}) lets you build modern, secure multi-point virtualized networks of almost any type. From robust peer-to-peer networking to multi-cloud mesh infrastructure, we enable global connectivity with the simplicity of a local network."
 
 pre_make_target() {
-  export CC="clang"
-  export CXX="clang++"
-  export CFLAGS="--target=${TARGET_NAME} --sysroot=${SYSROOT_PREFIX} ${CFLAGS//-mabi=lp64/}"
-  export CXXFLAGS="${CFLAGS}"
-  export TARGET_NAME
   cd ..
 }
 
