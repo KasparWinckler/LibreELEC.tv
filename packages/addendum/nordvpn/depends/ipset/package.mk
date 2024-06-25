aca66c99dfa053fa7d921741f0e0a32b80bf4fb6
Kaspar Winckler <155115050+KasparWinckler@users.noreply.github.com>
2024-06-25 08:19:14 +0200

    ipset: version 7.22

--- packages/addendum/nordvpn/depends/ipset/package.mk
+++ packages/addendum/nordvpn/depends/ipset/package.mk
@@ -1,6 +1,6 @@
 PKG_NAME="ipset"
-PKG_VERSION="7.21"
-PKG_SHA256="e2c6ce4fcf3acb3893ca5d35c86935f80ad76fc5ccae601185842df760e0bc69"
+PKG_VERSION="7.22"
+PKG_SHA256="f6ac5a47c3ef9f4c67fcbdf55e791cbfe38eb0a4aa1baacd12646a140abacdd9"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://ipset.netfilter.org/"
 PKG_URL="https://ipset.netfilter.org/ipset-${PKG_VERSION}.tar.bz2"
