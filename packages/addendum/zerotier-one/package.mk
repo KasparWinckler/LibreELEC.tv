a3ee4f8fddef9af8735619166624cdc4a31eecad
Kaspar Winckler <155115050+KasparWinckler@users.noreply.github.com>
2024-09-03 23:36:32 +0200

    zerotier-one: version 1.14.0 and addon (3)

--- packages/addendum/zerotier-one/package.mk
+++ packages/addendum/zerotier-one/package.mk
@@ -1,7 +1,7 @@
 PKG_NAME="zerotier-one"
 PKG_VERSION="1.14.0"
 PKG_SHA256="7191623a81b0d1b552b9431e8864dd3420783ee518394ac1376cee6aaf033291"
-PKG_REV="2"
+PKG_REV="3"
 PKG_LICENSE="BSL1.1"
 PKG_SITE="https://www.zerotier.com/"
 PKG_URL="https://github.com/zerotier/ZeroTierOne/archive/${PKG_VERSION}.tar.gz"
@@ -16,6 +16,12 @@ PKG_SHORTDESC="${PKG_ADDON_NAME}: securely connect any device, anywhere"
 PKG_LONGDESC="${PKG_ADDON_NAME} (${PKG_VERSION}) lets you build modern, secure multi-point virtualized networks of almost any type. From robust peer-to-peer networking to multi-cloud mesh infrastructure, we enable global connectivity with the simplicity of a local network."
 
 pre_make_target() {
+  export CC="clang"
+  export CXX="clang++"
+  export CFLAGS="--target=${TARGET_NAME} --sysroot=${SYSROOT_PREFIX}"
+  export CXXFLAGS="${CFLAGS}"
+  export TARGET_NAME
+  #echo $TARGET
   cd ..
 }
 
