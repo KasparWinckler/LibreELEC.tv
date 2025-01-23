99d61b8ea61ff579cb5250ab3202a5ccf5c04d30
Kaspar Winckler <155115050+KasparWinckler@users.noreply.github.com>
2025-01-23 22:07:01 +0100

    libtelio: version 5.1.6

--- packages/addendum/nordvpn/depends/libtelio/package.mk
+++ packages/addendum/nordvpn/depends/libtelio/package.mk
@@ -1,6 +1,6 @@
 PKG_NAME="libtelio"
-PKG_VERSION="5.0.0"
-PKG_SHA256="718d179232b9102ce15de72543c7e5c2eb2f3a02eeda4d914b79c305e70743ea"
+PKG_VERSION="5.1.6"
+PKG_SHA256="09bab5e32079d5b6314275df4e247ece9c57f7ee4db5a108ff89fd38c56fdb04"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/NordSecurity/libtelio"
 PKG_URL="https://github.com/NordSecurity/libtelio/archive/refs/tags/v${PKG_VERSION}.tar.gz"
