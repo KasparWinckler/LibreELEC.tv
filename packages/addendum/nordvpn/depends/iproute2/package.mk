a1995e1dd41779eaeea815c8c3a89a8cce7b4d76
Kaspar Winckler <155115050+KasparWinckler@users.noreply.github.com>
2025-04-02 17:42:29 +0200

    iproute2: version 6.14.0

--- packages/addendum/nordvpn/depends/iproute2/package.mk
+++ packages/addendum/nordvpn/depends/iproute2/package.mk
@@ -1,6 +1,6 @@
 PKG_NAME="iproute2"
-PKG_VERSION="6.13.0"
-PKG_SHA256="86fa7f3a58f57d1947835309d64be62d4135088692168735a0f074abcc667502"
+PKG_VERSION="6.14.0"
+PKG_SHA256="3d834c70aaa7ab588b34871fe8ec8fcaeca60019b3ced730dd02b81346e0c57e"
 PKG_LICENSE="GPLv2"
 PKG_SITE="https://wiki.linuxfoundation.org/networking/iproute2"
 PKG_URL="https://mirrors.edge.kernel.org/pub/linux/utils/net/iproute2/iproute2-${PKG_VERSION}.tar.gz"
