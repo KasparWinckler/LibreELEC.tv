7675fc6857056592d4e91340b4817c9ff3aca69c
Kaspar Winckler <155115050+KasparWinckler@users.noreply.github.com>
2025-06-29 20:41:55 +0200

    libtelio: version 5.4.0

--- packages/addendum/nordvpn/depends/libtelio/package.mk
+++ packages/addendum/nordvpn/depends/libtelio/package.mk
@@ -1,12 +1,16 @@
 PKG_NAME="libtelio"
-PKG_VERSION="5.1.9"
-PKG_SHA256="7773316a792553b7440241484ea884054de06b05ca9a65aba46dbe0a65ac0526"
+PKG_VERSION="5.4.0"
+PKG_SHA256="a4a8e7fee5e32f5bc0deed7fa0e6cca320993c65aada127e91ed98ed13c04da1"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/NordSecurity/libtelio"
 PKG_URL="https://github.com/NordSecurity/libtelio/archive/refs/tags/v${PKG_VERSION}.tar.gz"
 PKG_DEPENDS_TARGET="toolchain cargo:host"
 PKG_LONGDESC="libtelio"
 PKG_TOOLCHAIN="manual"
+
+configure_target() {
+  cargo update home@0.5.11 --precise 0.5.9
+}
 
 make_target() {
   BYPASS_LLT_SECRETS=1 cargo build --target ${TARGET_NAME} --release
