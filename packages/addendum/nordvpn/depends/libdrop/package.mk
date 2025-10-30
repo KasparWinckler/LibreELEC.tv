9a46d3e3053d04d8cf1891226a8739c86e81f198
Kaspar Winckler <155115050+KasparWinckler@users.noreply.github.com>
2025-10-30 16:44:22 +0100

    libdrop: version 9.0.0

--- packages/addendum/nordvpn/depends/libdrop/package.mk
+++ packages/addendum/nordvpn/depends/libdrop/package.mk
@@ -1,6 +1,6 @@
 PKG_NAME="libdrop"
-PKG_VERSION="8.2.4"
-PKG_SHA256="3025ba70bf5b5aec5b1f6cde75f44c3f5130f11aa7b77b30fe02731761aa36b2"
+PKG_VERSION="9.0.0"
+PKG_SHA256="e812c05a037e7255f6b7a3eda356ef651218c662351bc7958926e793a2d8a732"
 PKG_LICENSE="GPLv3"
 PKG_SITE="https://github.com/NordSecurity/libdrop"
 PKG_URL="https://github.com/NordSecurity/libdrop/archive/refs/tags/v${PKG_VERSION}.tar.gz"
